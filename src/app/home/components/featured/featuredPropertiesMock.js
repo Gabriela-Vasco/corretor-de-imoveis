a1021ae9e143ea2fcc61fcebd44b233cba498c12
Gabriela <gabrielavs1226@gmail.com>
2024-10-17 23:10:32 -0300

    feat: animated carousel

---
+++ src/app/home/components/featured/featuredPropertiesMock.js
@@ -0,0 +1,68 @@
+export const featuredPropertiesMock = [
+	{
+		title: "Apartamento de exemplo",
+		neighborhood: "Centro",
+		image:
+			"https://media.istockphoto.com/id/1255835530/photo/modern-custom-suburban-home-exterior.jpg?s=612x612&w=0&k=20&c=0Dqjm3NunXjZtWVpsUvNKg2A4rK2gMvJ-827nb4AMU4=",
+		code: "547",
+		price: 100000,
+		bedrooms: 3,
+		bathrooms: 2,
+		garage: 1,
+	},
+	{
+		title: "Apartamento de exemplo",
+		neighborhood: "Centro",
+		image:
+			"https://media.istockphoto.com/id/1255835530/photo/modern-custom-suburban-home-exterior.jpg?s=612x612&w=0&k=20&c=0Dqjm3NunXjZtWVpsUvNKg2A4rK2gMvJ-827nb4AMU4=",
+		code: "547",
+		price: 100000,
+		bedrooms: 3,
+		bathrooms: 2,
+		garage: 1,
+	},
+	{
+		title: "Apartamento de exemplo",
+		neighborhood: "Centro",
+		image:
+			"https://media.istockphoto.com/id/1255835530/photo/modern-custom-suburban-home-exterior.jpg?s=612x612&w=0&k=20&c=0Dqjm3NunXjZtWVpsUvNKg2A4rK2gMvJ-827nb4AMU4=",
+		code: "547",
+		price: 100000,
+		bedrooms: 3,
+		bathrooms: 2,
+		garage: 1,
+	},
+	{
+		title: "Apartamento de exemplo 2",
+		neighborhood: "Centro",
+		image:
+			"https://media.istockphoto.com/id/1255835530/photo/modern-custom-suburban-home-exterior.jpg?s=612x612&w=0&k=20&c=0Dqjm3NunXjZtWVpsUvNKg2A4rK2gMvJ-827nb4AMU4=",
+		code: "547",
+		price: 100000,
+		bedrooms: 3,
+		bathrooms: 2,
+		garage: 1,
+	},
+	{
+		title: "Apartamento de exemplo 2",
+		neighborhood: "Centro",
+		image:
+			"https://media.istockphoto.com/id/1255835530/photo/modern-custom-suburban-home-exterior.jpg?s=612x612&w=0&k=20&c=0Dqjm3NunXjZtWVpsUvNKg2A4rK2gMvJ-827nb4AMU4=",
+		code: "547",
+		price: 100000,
+		bedrooms: 3,
+		bathrooms: 2,
+		garage: 1,
+	},
+	{
+		title: "Apartamento de exemplo 2",
+		neighborhood: "Centro",
+		image:
+			"https://media.istockphoto.com/id/1255835530/photo/modern-custom-suburban-home-exterior.jpg?s=612x612&w=0&k=20&c=0Dqjm3NunXjZtWVpsUvNKg2A4rK2gMvJ-827nb4AMU4=",
+		code: "547",
+		price: 100000,
+		bedrooms: 3,
+		bathrooms: 2,
+		garage: 1,
+	},
+];
--- src/app/home/components/featured/index.vue
+++ src/app/home/components/featured/index.vue
@@ -2,78 +2,157 @@
 	<div class="d-flex flex-column align-center py-16">
 		<h3 class="featuredTitle mb-12">Imóveis em destaque</h3>
 		<div
-			v-if="featuredProperties?.length"
-			class="d-flex align-center justify-space-between w-100"
+			v-if="visibleProperties.length"
+			class="d-flex align-center justify-center py-2"
 		>
 			<v-btn
 				icon
-				variant="flat"
+				variant="text"
 				size="90px"
 				class="d-flex align-center justify-center ml-2 pr-2"
+				:disabled="currentIndex === 0"
+				@click="prevProperty"
 			>
-				<v-icon size="120px" color="primary">mdi-menu-left</v-icon>
+				<v-icon size="120px" :color="currentIndex === 0 ? 'gray' : 'primary'">
+					mdi-menu-left
+				</v-icon>
 			</v-btn>
-			<PropertyCard
-				v-for="(featuredProperty, index) of featuredProperties"
-				:key="index"
-				:featured-property="featuredProperty"
-			/>
+
+			<div class="d-flex justify-center align-center w-100 overflow-hidden py-5">
+				<div ref="cardsContainer" class="property-cards mx-8">
+					<PropertyCard
+						v-for="property in visibleProperties"
+						:key="property.code"
+						:featured-property="property"
+						class="property-card"
+					/>
+				</div>
+			</div>
+
 			<v-btn
 				icon
-				variant="flat"
+				variant="text"
 				size="90px"
 				class="d-flex align-center justify-center mr-2 pl-2"
+				:disabled="currentIndex + visibleCount >= featuredProperties.length"
+				@click="nextProperty"
 			>
-				<v-icon size="120px" color="primary">mdi-menu-right</v-icon>
+				<v-icon
+					size="120px"
+					:color="
+						currentIndex + visibleCount >= featuredProperties.length
+							? 'gray'
+							: 'primary'
+					"
+				>
+					mdi-menu-right
+				</v-icon>
 			</v-btn>
 		</div>
 	</div>
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref, computed, nextTick } from "vue";
+import { animate } from "motion";
+import { featuredPropertiesMock } from "./featuredPropertiesMock";
 import PropertyCard from "@/components/PropertyCard";
 import { type FeaturedProperty } from "~/types/FeaturedProperty";
 
-const featuredProperties = ref<FeaturedProperty[]>([
-	{
-		title: "Apartamento de exemplo",
-		neighborhood: "Centro",
-		image:
-			"https://media.istockphoto.com/id/1255835530/photo/modern-custom-suburban-home-exterior.jpg?s=612x612&w=0&k=20&c=0Dqjm3NunXjZtWVpsUvNKg2A4rK2gMvJ-827nb4AMU4=",
-		code: "547",
-		price: 100000,
-		bedrooms: 3,
-		bathrooms: 2,
-		garage: 1,
-	},
-	{
-		title: "Apartamento de exemplo",
-		neighborhood: "Centro",
-		image:
-			"https://media.istockphoto.com/id/1255835530/photo/modern-custom-suburban-home-exterior.jpg?s=612x612&w=0&k=20&c=0Dqjm3NunXjZtWVpsUvNKg2A4rK2gMvJ-827nb4AMU4=",
-		code: "547",
-		price: 100000,
-		bedrooms: 3,
-		bathrooms: 2,
-		garage: 1,
-	},
-	{
-		title: "Apartamento de exemplo",
-		neighborhood: "Centro",
-		image:
-			"https://media.istockphoto.com/id/1255835530/photo/modern-custom-suburban-home-exterior.jpg?s=612x612&w=0&k=20&c=0Dqjm3NunXjZtWVpsUvNKg2A4rK2gMvJ-827nb4AMU4=",
-		code: "547",
-		price: 100000,
-		bedrooms: 3,
-		bathrooms: 2,
-		garage: 1,
-	},
-]);
+const featuredProperties = ref<FeaturedProperty[]>(featuredPropertiesMock);
+const currentIndex = ref(0);
+const visibleCount = 3;
+
+const visibleProperties = computed(() => {
+	return featuredProperties.value.slice(
+		currentIndex.value,
+		currentIndex.value + visibleCount,
+	);
+});
+
+const cardsContainer = ref<HTMLElement | null>(null);
+
+const prevProperty = () => {
+	if (currentIndex.value > 0) {
+		animateCards("left");
+
+		currentIndex.value = Math.max(currentIndex.value - visibleCount, 0);
+
+		nextTick(() => {
+			animateCardsIn("left");
+		});
+	}
+};
+
+const nextProperty = () => {
+	if (currentIndex.value + visibleCount < featuredProperties.value.length) {
+		animateCards("right");
+
+		currentIndex.value = Math.min(
+			currentIndex.value + visibleCount,
+			featuredProperties.value.length - visibleCount,
+		);
+
+		nextTick(() => {
+			animateCardsIn("right");
+		});
+	}
+};
+
+const animateCards = (direction: string) => {
+	const slideDistance = direction === "right" ? -100 : 100;
+	if (cardsContainer.value) {
+		const cardElements = Array.from(cardsContainer.value.children);
+
+		return animate(
+			cardElements,
+			{
+				transform: `translateX(${slideDistance}px)`,
+				opacity: 0,
+			},
+			{
+				duration: 0.5,
+				easing: "ease-in-out",
+			},
+		);
+	}
+};
+
+const animateCardsIn = (direction: string) => {
+	const slideDistance = direction === "right" ? 100 : -100;
+	if (cardsContainer.value) {
+		const cardElements = Array.from(cardsContainer.value.children);
+
+		animate(
+			cardElements,
+			{
+				transform: [`translateX(${slideDistance}px)`, "translateX(0)"],
+				opacity: [0, 1],
+			},
+			{
+				duration: 0.5,
+				easing: "ease-in-out",
+			},
+		);
+	}
+};
 </script>
 
-<style lang="scss" scoped>
+<style scoped lang="scss">
 .featuredTitle {
 	font-size: 32px;
+}
+
+.property-cards {
+	display: flex;
+	gap: 40px;
+	transition:
+		transform 0.5s ease-in-out,
+		opacity 0.5s ease-in-out;
+}
+
+.property-card:hover {
+	cursor: pointer;
+	box-shadow: 0 8px 16px rgba(0, 0, 0, 0.15);
 }
 </style>
