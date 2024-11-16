b0436b66e032575aae3e0c47a6e8a6bb55325ac3
Gabriela <gabrielavs1226@gmail.com>
2024-11-16 12:09:08 -0300

    fix: using gcp keys from env file

--- src/server/api/properties.ts
+++ src/server/api/properties.ts
@@ -18,6 +18,11 @@ export default defineEventHandler(async (event) => {
 		serviceAccountAuth,
 	);
 
+	const CLIENT_ID = process.env.GOOGLE_DRIVE_CLIENT_ID;
+	const CLIENT_SECRET = process.env.GOOGLE_DRIVE_CLIENT_SECRET;
+	const REDIRECT_URI = process.env.GOOGLE_DRIVE_REDIRECT_URI;
+	const REFRESH_TOKEN = process.env.GOOGLE_DRIVE_REFRESH_TOKEN;
+
 	const oauth2Client = new google.auth.OAuth2(
 		CLIENT_ID,
 		CLIENT_SECRET,
